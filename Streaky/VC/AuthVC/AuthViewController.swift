5926098ac97f33962b55c6ca011b733ea67cf7b5
FatmaBuyabes <buyabesfatemah@gmail.com>
2024-05-22 14:55:02 +0300

    Auth and Login added

---
+++ Streaky/VC/AuthVC/AuthViewController.swift
@@ -0,0 +1,120 @@
+//  LoginViewController.swift
+//  Streaky
+//
+//  Created by Fatma Buyabes on 21/05/2024.
+//
+
+import UIKit
+import SnapKit
+
+class AuthViewController: UIViewController {
+
+    var background = UIImageView()
+    var logoImageView = UIImageView()
+    var welcomeLabel = UILabel()
+    var descriptionLabel = UILabel()
+    var loginButton = UIButton()
+    var signUpButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUi()
+        setupConstraints()
+        // Do any additional setup after loading the view.
+    }
+    
+    func setupUi() {
+        // Set up the background image
+        background.image = UIImage(named: "Background") // Ensure this image is in your assets
+        background.contentMode = .scaleAspectFill
+        background.clipsToBounds = true
+        view.addSubview(background)
+        
+        // Set up the logo image
+        logoImageView.image = UIImage(named: "Logo") // Ensure this image is in your assets
+        logoImageView.contentMode = .scaleAspectFit
+        view.addSubview(logoImageView)
+        
+        // Set up the welcome label
+        welcomeLabel.text = "Welcome"
+        welcomeLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        welcomeLabel.textColor = .white
+        welcomeLabel.textAlignment = .left
+        view.addSubview(welcomeLabel)
+        
+        // Set up the description label
+        descriptionLabel.text = "Unlock exclusive benefits and discover a world of perks at your fingertips with Streaky"
+        descriptionLabel.font = UIFont.systemFont(ofSize: 16)
+        descriptionLabel.textColor = .white
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.textAlignment = .left
+        view.addSubview(descriptionLabel)
+        
+        // Set up the login button
+        loginButton.setTitle("Login to your account", for: .normal)
+        loginButton.backgroundColor = #colorLiteral(red: 0.3071659207, green: 0.1487102509, blue: 0.5210966468, alpha: 1)
+        loginButton.layer.cornerRadius = 24
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        view.addSubview(loginButton)
+        
+        // Set up the sign-up button
+        signUpButton.setTitle("Sign up", for: .normal)
+        signUpButton.backgroundColor = .clear
+        signUpButton.setTitleColor(.white, for: .normal)
+        signUpButton.layer.borderWidth = 2
+        signUpButton.layer.borderColor = #colorLiteral(red: 0.3071659207, green: 0.1487102509, blue: 0.5210966468, alpha: 1)
+        signUpButton.layer.cornerRadius = 24
+        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+        view.addSubview(signUpButton)
+    }
+    
+    func setupConstraints() {
+        background.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        logoImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(230)
+            make.width.height.equalTo(160)
+        }
+        
+        welcomeLabel.snp.makeConstraints { make in
+            make.top.equalTo(logoImageView.snp.bottom).offset(10)
+            make.leading.equalTo(descriptionLabel.snp.leading)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview().offset(-100)
+            make.top.equalTo(welcomeLabel.snp.bottom).offset(10)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+        
+        loginButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(60)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.height.equalTo(50)
+        }
+        
+        signUpButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(loginButton.snp.bottom).offset(20)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.height.equalTo(50)
+        }
+    }
+    
+    @objc func loginButtonTapped() {
+            let loginVC = LoginViewController()
+            self.navigationController?.pushViewController(loginVC, animated: true)
+        }
+
+    @objc func signUpButtonTapped() {
+        // Handle sign-up button tap
+        let loginVC = LoginViewController()
+        self.navigationController?.pushViewController(loginVC, animated: true)
+    }
+}
---
+++ Streaky/VC/AuthVC/LoginViewController.swift
@@ -0,0 +1,198 @@
+//  LoginViewController.swift
+//  Streaky
+//
+//  Created by Fatma Buyabes on 20/05/2024.
+//
+
+import UIKit
+import SnapKit
+
+class LoginViewController: UIViewController {
+    
+    var emailTextField: UITextField!
+    var passwordTextField: UITextField!
+    var backgroundImageView: UIImageView!
+    var welcomeLabel: UILabel!
+    var signUpLabel: UILabel!
+    var loginButton: UIButton!
+    var registerButton: UIButton!
+    var logoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Log In"
+        setupUi()
+        configureNavigationBar()
+        setupConstraints()
+    }
+    
+    func setupUi() {
+        // Set up the background image
+        backgroundImageView = UIImageView()
+        backgroundImageView.image = UIImage(named: "Background") // Make sure this image is in your assets
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.clipsToBounds = true
+        view.addSubview(backgroundImageView)
+        
+        welcomeLabel = UILabel()
+        welcomeLabel.text = "Welcome to"
+        welcomeLabel.font = UIFont.systemFont(ofSize: 45, weight: .bold)
+        welcomeLabel.textColor = .white
+        welcomeLabel.textAlignment = .center
+        welcomeLabel.layer.shadowColor = UIColor.black.cgColor
+        welcomeLabel.layer.shadowOffset = CGSize(width: 2, height: 2)
+        welcomeLabel.layer.shadowRadius = 4
+        welcomeLabel.layer.shadowOpacity = 0.7
+        view.addSubview(welcomeLabel)
+        
+        signUpLabel = UILabel()
+        signUpLabel.text = "Streaky"
+        signUpLabel.font = UIFont.systemFont(ofSize: 45, weight: .bold)
+        signUpLabel.textColor = .white
+        signUpLabel.textAlignment = .center
+        signUpLabel.layer.shadowColor = UIColor.black.cgColor
+        signUpLabel.layer.shadowOffset = CGSize(width: 2, height: 2)
+        signUpLabel.layer.shadowRadius = 4
+        signUpLabel.layer.shadowOpacity = 0.7
+        view.addSubview(signUpLabel)
+        
+        emailTextField = UITextField()
+        emailTextField.placeholder = "Email"
+        emailTextField.borderStyle = .roundedRect
+        view.addSubview(emailTextField)
+        
+        passwordTextField = UITextField()
+        passwordTextField.placeholder = "Password"
+        passwordTextField.layer.cornerRadius = 30
+        passwordTextField.borderStyle = .roundedRect
+        passwordTextField.isSecureTextEntry = true
+        view.addSubview(passwordTextField)
+        
+        loginButton = UIButton(type: .system)
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.backgroundColor = #colorLiteral(red: 0.2706783712, green: 0.1171713695, blue: 0.4809373021, alpha: 1)
+        loginButton.layer.cornerRadius = 24
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        view.addSubview(loginButton)
+        
+        registerButton = UIButton(type: .system)
+        registerButton.setTitle("No account? Register now", for: .normal)
+        registerButton.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        registerButton.setTitleColor(.white, for: .normal)
+        registerButton.backgroundColor = .clear
+        registerButton.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
+        view.addSubview(registerButton)
+        
+        logoImageView = UIImageView()
+        logoImageView.image = UIImage(named: "Logo")
+        logoImageView.contentMode = .scaleAspectFit
+        view.addSubview(logoImageView)
+    }
+    
+    func setupConstraints() {
+        backgroundImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        welcomeLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(80)
+            make.centerX.equalToSuperview()
+        }
+        
+        signUpLabel.snp.makeConstraints { make in
+            make.top.equalTo(welcomeLabel.snp.bottom).offset(6)
+            make.centerX.equalToSuperview()
+        }
+        
+        emailTextField.snp.makeConstraints { make in
+            make.top.equalTo(signUpLabel.snp.bottom).offset(40)
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.height.equalTo(44)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.top.equalTo(emailTextField.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.height.equalTo(44)
+        }
+        
+        loginButton.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(30)
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.height.equalTo(50)
+        }
+        
+        registerButton.snp.makeConstraints { make in
+            make.top.equalTo(loginButton.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(30)
+        }
+        
+        logoImageView.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(200)
+            make.height.equalTo(100)
+        }
+    }
+    
+    func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .clear
+        appearance.titleTextAttributes = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.systemFont(ofSize: 18, weight: .bold)
+        ]
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        // Add back button
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: self, action: #selector(backButtonTapped))
+        backButton.tintColor = .white
+        navigationItem.leftBarButtonItem = backButton
+    }
+    
+    @objc func backButtonTapped() {
+        navigationController?.popToRootViewController(animated: true)
+        let onboardingVC = AuthViewController()
+        self.navigationController?.pushViewController(onboardingVC, animated: false)
+    }
+    
+    @objc func loginButtonTapped() {
+        guard let email = emailTextField.text, !email.isEmpty else {
+            presentAlertWithTitle(title: "Error", message: "Email is required")
+            return
+        }
+        
+        guard let password = passwordTextField.text, !password.isEmpty else {
+            presentAlertWithTitle(title: "Error", message: "Password is required")
+            return
+        }
+        //let user = User(name: nil, email: email, password: password, gender:nil)
+
+        // Perform login action here
+        
+        // Navigate to the next screen after login
+        let nextVC = AuthViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+        
+        
+    
+    @objc func registerButtonTapped() {
+        // Navigate to the registration screen
+        let authVC = AuthViewController()
+        self.navigationController?.pushViewController(authVC, animated: true)
+    }
+    
+    func presentAlertWithTitle(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
