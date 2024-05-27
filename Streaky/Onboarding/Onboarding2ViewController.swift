f2ae175b4f842d4eaaa535c6de1464cfdf7ed866
FatmaBuyabes <buyabesfatemah@gmail.com>
2024-05-27 09:41:54 +0300

    Changed the screen after the onboarding 2 to Auth Screen

--- Streaky/Onboarding/Onboarding2ViewController.swift
+++ Streaky/Onboarding/Onboarding2ViewController.swift
@@ -144,7 +144,7 @@ class Onboarding2ViewController: UIViewController {
     }
     
     @objc private func nextButtonTapped() {
-        let nextVC = SignUpViewController()
-        navigationController?.pushViewController(nextVC, animated: true)
+        let AuthVC = AuthViewController()
+        navigationController?.pushViewController(AuthVC, animated: true)
     }
 }
