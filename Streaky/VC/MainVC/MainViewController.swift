2bce58b595188a05744ca4a42690cf2e87a223b6
FatmaBuyabes <buyabesfatemah@gmail.com>
2024-05-27 14:58:34 +0300

    Fixed TableView Controller --> Nawaf

--- Streaky/VC/MainVC/MainViewController.swift
+++ Streaky/VC/MainVC/MainViewController.swift
@@ -3,21 +3,28 @@ import SnapKit
 
 class MainViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set up the main view
-        view.backgroundColor = .white
-        
-        // Set up and add the StreaksViewController
-        let streaksViewController = StreaksTableViewController()
-        addChild(streaksViewController)
-        view.addSubview(streaksViewController.view)
-        streaksViewController.didMove(toParent: self)
-        
-        // Set up constraints for the streaksViewController's view using SnapKit
-        streaksViewController.view.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        view.backgroundColor = .white
+//        
+//        // Set up and add the StreaksViewController
+//        let streaksViewController = StreaksTableViewController()
+//        addChild(streaksViewController)
+//        view.addSubview(streaksViewController.view)
+//        streaksViewController.didMove(toParent: self)
+//        
+//        let todayQuestionViewController = TodayQuestionCollectionViewController()
+//        addChild(todayQuestionViewController)
+//        view.addSubview(todayQuestionViewController.view)
+//        todayQuestionViewController.didMove(toParent: self)
+//        
+//        streaksViewController.view.snp.makeConstraints { make in
+//            make.edges.equalToSuperview()
+//        }
+//        
+//        todayQuestionViewController.view.snp.makeConstraints { make in
+//            make.edges.equalToSuperview().offset(90)
+//        }
+//    }
 }
