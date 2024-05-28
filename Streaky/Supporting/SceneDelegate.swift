dd224277cb1d17dc6219de9a93e987026bcc1dbe
Faten <fatennald@gmail.com>
2024-05-28 10:04:31 +0300

    customize Tab Bar in the home page

--- Streaky/Supporting/SceneDelegate.swift
+++ Streaky/Supporting/SceneDelegate.swift
@@ -18,9 +18,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        // Window for the entery point or the app view
+//         Window for the entery point or the app view
 
-        window.rootViewController = UINavigationController(rootViewController: QuestionViewController())
+        window.rootViewController = UINavigationController(rootViewController: MainTabBarViewController())
+        
+       
 
 
 
