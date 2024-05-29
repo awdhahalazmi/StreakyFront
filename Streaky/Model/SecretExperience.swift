42a19bf62aa892b0ca52a9d980964d69f7d4a449
Faten <fatennald@gmail.com>
2024-05-29 09:32:48 +0300

    view the Secret Experience in the main screen

---
+++ Streaky/Model/SecretExperience.swift
@@ -0,0 +1,14 @@
+//
+//  SecretExperience.swift
+//  Streaky
+//
+//  Created by faten aldosari on 28/05/2024.
+//
+
+import Foundation
+struct SecretExperience {
+    let title: String
+    let imageName: String
+    let description: String
+    let streaks: Int
+}
